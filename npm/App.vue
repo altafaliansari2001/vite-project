3c9face96eb4809cb2e08ff73edeb2c3c68125b1
altaf ali <altafaliansari092001@gmail.com>
2024-11-28 20:59:00 +0530

    first commit

---
+++ npm/App.vue
@@ -0,0 +1,19 @@
+<!-- App.vue -->
+<template>
+  <div>
+    <h1>Hello, Vite with Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
+
---
+++ npm/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// Vite configuration
+export default defineConfig({
+  plugins: [vue()],
+});
+
